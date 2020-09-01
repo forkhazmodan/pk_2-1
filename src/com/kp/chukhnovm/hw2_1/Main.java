3293bb5068b15585bf2d35a3eb7783b5a5e93f60
Chukhnov Max <4myself.max@gmail.com>
2020-09-02 00:24:51 +0300

    Add some tips with rounding

--- src/com/kp/chukhnovm/hw2_1/Main.java
+++ src/com/kp/chukhnovm/hw2_1/Main.java
@@ -2,11 +2,17 @@ package com.kp.chukhnovm.hw2_1;
 
 import com.kp.chukhnovm.hw2_1.shapes.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Triangle tr1 = new Triangle(new Point(0, 0), new Point(2, 5), new Point(0, 2));
+
         Circle c1 = new Circle(new Point(0, 0), new Point(0, 1));
         Circle c2 = new Circle(new Point(0, 0), new Point(0, 2));
         Circle c3 = new Circle(new Point(0, 0), new Point(0, 3));
@@ -31,5 +37,17 @@ public class Main {
         table.addShape(c4); // After removing we can add shapes again
 
         System.out.println(table.toString());
+
+
+        // Round testing
+        // How about rounding like this?
+        Locale.setDefault(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setRoundingMode(RoundingMode.UP);
+
+        Triangle tr2 = new Triangle(new Point(0, 0), new Point(0.02, 0.05), new Point(0, 0.02));
+        System.out.println(tr2.getArea());
+        System.out.println(BigDecimal.valueOf(tr2.getArea()));
+        System.out.println(df.format(tr2.getArea()));
     }
 }
